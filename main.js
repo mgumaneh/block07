97c67cacf5755908f3a666b37a21f7cacfb2ccc2
Mahamadou Gumaneh <mg@Mahamadous-Air.home>
2024-03-04 16:33:09 -0800

    First draft of block07

---
+++ main.js
@@ -0,0 +1,16 @@
+/*
+1.pseudocode steps 
+1a.Link index.html with main.js
+ 2.make a string for the user saying: You have received this message because you have been chosen to open an important vault. Here is the secret combination:
+ 3.choose three variables
+ 4.Add comments throughout the code to explain each step in the process.
+ 5.Create a dialog box 
+ 6.push it to github
+*/
+//The message is equal to the variable constant info
+const info = "You have received this message because you have been chosen to open an important vault. Here is the secret combination:";
+//These three variable constants are equal to the numbers that results up to the secret combination
+const num1 = 8+2;
+const num2 = 2*20;
+const num3 = 80-41;
+alert(info+" " + num1+ " "+ num2+ " " + num3+ " ");
